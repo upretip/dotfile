8b5eb7bb2f50d6f7abba25580d024147c31ae5a2
pupreti <pupreti@ucsc.edu>
2020-03-18 10:15:03 -0700

    initialize dotfile

---
+++ dotfile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew analytics off
