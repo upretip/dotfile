e78fc43e2e79f6472a7a8852dedb118dfd312bd0
pupreti <pupreti@ucsc.edu>
2020-04-04 21:04:09 -0700

    add brew and brew casks

--- dotfile.sh
+++ dotfile.sh
@@ -7,5 +7,17 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/mas
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew analytics off
 
+# brew
+brew install gcc
+brew install python
+brew install r
+brew install postgres
+brew install unixodbc
 
+
+# brew cask
 brew cask install docker
+brew cask install iterm2
+brew cask install visual-studio-code
+brew cask install rstudio
+brew cask install firefox
