7d46f044dc68034ac84d4fce805577fb7a889c05
upretip <parashupreti@gmail.com>
2021-06-09 14:45:40 -0700

    change homebrew install from ruby to shell

--- dotfile.sh
+++ dotfile.sh
@@ -5,7 +5,7 @@ xcode-select --install
 
 echo "Installing Homebrew"
 #Brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew analytics off
 brew upgrade
 brew update
