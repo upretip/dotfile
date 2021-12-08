3d85bc02962ea189d83db12a1493f945d36b245d
pupreti <parashupreti@gmail.com>
2021-12-07 17:45:36 -0800

    added spaceship theme

--- dotfile.sh
+++ dotfile.sh
@@ -6,6 +6,11 @@ xcode-select --install
 echo "Installing Oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#install zsh theme
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+
+
 echo "Installing Homebrew"
 #Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
